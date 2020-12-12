e2d74ead8a825d60c137bce057274a02103e1751
이수연 <sooyeon@isuyeon-ui-MacBookAir.local>
2020-12-12 19:24:17 +0900

    나의정보 수정하는 API추가

---
+++ pages/api/my/update.ts
@@ -0,0 +1,29 @@
+import { NextApiHandler } from "next";
+import { query } from "../../../lib/db";
+
+const handler: NextApiHandler = async (req, res) => {
+  console.log("req.body = ", req.body);
+  const { id, appealText, content } = req.body;
+  try {
+    if (!id || !appealText || !content) {
+      return res
+        .status(400)
+        .json({ message: "`id`,`appealText`, and `content` are all required" });
+    }
+
+    const results = await query(
+      `
+      UPDATE beInvested
+      SET appealText = ?, content = ?
+      WHERE user_id = (SELECT id FROM users WHERE userId = ?)
+      `,
+      [appealText, content, id]
+    );
+
+    return res.json(results);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+export default handler;
