196a222060ab962abc54762573bfdddcb8cbabd8
이수연 <sooyeon@isuyeon-ui-MacBookAir.local>
2020-12-12 18:47:50 +0900

    로그인 API생성

---
+++ pages/api/user/login.ts
@@ -0,0 +1,32 @@
+import { NextApiHandler } from "next";
+import { query } from "../../../lib/db";
+
+const handler: NextApiHandler = async (req, res) => {
+  const { id, password } = req.body;
+  try {
+    if (!id || !password) {
+      return res
+        .status(400)
+        .json({ message: "`id` and `password` are both required" });
+    }
+
+    const results = await query(
+      `
+        SELECT userId
+        FROM users
+        WHERE userId = ? AND ? = (SELECT cast(AES_DECRYPT(UNHEX(password), '${process.env.MYSQL_HEX_KEY}') as char(100)) FROM users where userId=?)
+      `,
+      [id, password, id]
+    );
+
+    // @ts-ignore
+    const resultMessage = results && results.length > 0 ? "success!" : "fail";
+    // @ts-ignore
+    const resultStatus = results && results.length > 0 ? 200 : 404;
+    return res.status(resultStatus).json({ resultMessage });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+export default handler;
