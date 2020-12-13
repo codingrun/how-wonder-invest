15ab385c0e7c87f4430533ef3068badb8aa4ddc4
이수연 <sooyeon@isuyeon-ui-MacBookAir.local>
2020-12-13 17:27:33 +0900

    투자자인지아닌지 응답에 추가

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -12,7 +12,7 @@ const handler: NextApiHandler = async (req, res) => {
 
     const results = await query(
       `
-        SELECT userId
+        SELECT userId, isInvestor
         FROM users
         WHERE userId = ? AND ? = (SELECT cast(AES_DECRYPT(UNHEX(password), '${process.env.MYSQL_HEX_KEY}') as char(100)) FROM users where userId=?)
       `,
@@ -23,7 +23,11 @@ const handler: NextApiHandler = async (req, res) => {
     const resultMessage = results && results.length > 0 ? "success!" : "fail";
     // @ts-ignore
     const resultStatus = results && results.length > 0 ? 200 : 404;
-    return res.status(resultStatus).json({ message: resultMessage });
+
+    return res.status(resultStatus).json({
+      message: resultMessage,
+      isInvestor: results[0].isInvestor || false,
+    });
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
